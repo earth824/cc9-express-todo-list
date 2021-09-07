2b514b5fa2176c978957e284a564d70b5e9621ad
pisutthee <pisut.flyinggiraffe@gmail.com>
2021-09-07 18:42:06 +0700

    config controller

---
+++ controllers/todoController.js
@@ -0,0 +1,17 @@
+exports.getAllTodo = (req, res, next) => {
+  res.json({ message: 'GET All Todo' });
+};
+
+exports.createTodo = (req, res, next) => {};
+exports.updateTodo = (req, res, next) => {};
+
+exports.deleteTodo = (req, res, next) => {
+  // read todo
+  // find todo to delete
+  // if found
+  // dellete todo
+  // save todo
+  // send response
+  // not found
+  // send error response
+};
